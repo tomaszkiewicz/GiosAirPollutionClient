9afdbceffc9206465aac71f66d3ac33f7a506880
luktom <luktom@luktom.net>
2017-02-24 18:03:51 +0100

    Update assembly description

--- GiosAirPollutionClient/Properties/AssemblyInfo.cs
+++ GiosAirPollutionClient/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GiosAirPollutionClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for getting data about air pollution from Główny Inspektorat Ochrony Środowiska")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GiosAirPollutionClient")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Łukasz Tomaszkiewicz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
